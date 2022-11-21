d3dc121e51dd545d56f869e5716217c154f2a0e3
Pierre <36824955+PierreDmyy@users.noreply.github.com>
2022-11-21 07:26:53 +0100

    first

---
+++ src/api/accronymes/controllers/accronymes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accronymes controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::accronymes.accronymes');
---
+++ src/api/accronymes/routes/accronymes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accronymes router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::accronymes.accronymes');
