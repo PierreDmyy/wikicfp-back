ad7a3c924adefb08c44f4ac7c862011f943d11f4
Pierre <36824955+PierreDmyy@users.noreply.github.com>
2022-11-22 07:56:44 +0100

    add key

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['afe7405d67f206b6', '63e90c0711630990']),
   },
 });
